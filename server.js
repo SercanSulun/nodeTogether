aaaf8f07840b155c8d375bce05194e097278b03d
SercanSulun <sercansulun@gmail.com>
2016-09-14 20:27:51 +0200

    create server that listens on 127.0.0.1:8989

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 //server.js
 var express = require('express');
+var host = "127.0.0.1";
+var port = "8989";
+
+var app = express();
+app.listen(port, host);
+console.log("Server started on 127.0.0.1:8989");
