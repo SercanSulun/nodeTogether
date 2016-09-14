2da7fee24a7b780f6e2131a2cf1a45049c3a810b
SercanSulun <sercansulun@gmail.com>
2016-09-14 21:30:10 +0200

    solves problem before heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //server.js
 var express = require('express');
 var routes = require('./app/index.js');
-var host = precess.env.HOST || "0.0.0.0";
-var port = precess.env.PORT || "8989";
+var host = process.env.HOST || "0.0.0.0";
+var port = process.env.PORT || "8989";
 
 var app = express();
 app.use(routes);
